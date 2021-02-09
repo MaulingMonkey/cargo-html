aab5e5b342a7aa4d934231b048ac993f3659185e
MaulingMonkey <git@maulingmonkey.com>
2021-02-09 14:42:19 -0800

    support fd_read(stdin, ...)

--- hello-world/hello-world.rs
+++ hello-world/hello-world.rs
@@ -1,3 +1,12 @@
+use std::io::Write;
+
 fn main() {
-    println!("Hello, world!");
+    print!("What's your name? ");
+    std::io::stdout().flush().unwrap();
+
+    let mut name = String::new();
+    std::io::stdin().read_line(&mut name).unwrap();
+    let name = name.trim();
+
+    println!("Hello, {}!", name);
 }
