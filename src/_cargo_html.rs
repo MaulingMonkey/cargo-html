01b69fe34b78cead38ac1370600043e686aacef7
MaulingMonkey <git@maulingmonkey.com>
2021-02-09 12:01:43 -0800

    tweak embedding setup

--- src/_cargo_html.rs
+++ src/_cargo_html.rs
@@ -74,14 +74,13 @@ fn main() {
 
     println!("\u{001B}[30;102m                            Bundling HTML pages                             \u{001B}[0m");
 
-    let template_script_js  = include_str!("../template/script.js");
-    let script_placeholder  = "{SCRIPT}";
+    let script_placeholder  = "{BASE64_WASM32}";
 
     for config in configs.iter().copied() {
         let target_dir = Path::new("target").join("wasm32-wasi").join(config.as_str());
         for (_ty, target) in targets.iter() {
             let template_html = include_str!("../template/console-crate.html").replace("{CONFIG}", config.as_str()).replace("{CRATE_NAME}", &target);
-            let script_placeholder_idx = template_html.find(script_placeholder).expect("template missing {SCRIPT}");
+            let script_placeholder_idx = template_html.find(script_placeholder).expect("template missing {BASE64_WASM32}");
 
             let target_wasm = target_dir.join(format!("{}.wasm", target));
             let target_wasm = std::fs::read(&target_wasm).unwrap_or_else(|err| fatal!("unable to read `{}`: {}", target_wasm.display(), err));
@@ -90,10 +89,9 @@ fn main() {
             let target_html = target_dir.join(format!("{}.html", target));
             status!("Generating", "{}", target_html.display());
             mmrbi::fs::write_if_modified_with(target_html, |o| {
-                writeln!(o, "{}", &template_html[..script_placeholder_idx])?;
-                writeln!(o, "{}", template_script_js)?;
-                writeln!(o, "exec_base64_wasm({:?});", target_wasm)?;
-                writeln!(o, "{}", &template_html[(script_placeholder_idx + script_placeholder.len())..])?;
+                write!(o, "{}", &template_html[..script_placeholder_idx])?;
+                write!(o, "{:?}", target_wasm)?;
+                write!(o, "{}", &template_html[(script_placeholder_idx + script_placeholder.len())..])?;
                 Ok(())
             }).unwrap_or_else(|err| fatal!("unable to fully write HTML file: {}", err));
         }
