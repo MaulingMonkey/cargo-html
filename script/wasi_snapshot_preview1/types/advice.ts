3c6a68686229330fd292bab77891fed73f3f0d6d
MaulingMonkey <git@maulingmonkey.com>
2021-02-22 20:14:26 -0800

    Advice

---
+++ script/wasi_snapshot_preview1/types/advice.ts
@@ -0,0 +1,26 @@
+/**
+ * File or memory access pattern advisory information.
+ *
+ * ### See Also
+ *
+ * * [WASI standard documentation](https://github.com/WebAssembly/WASI/blob/main/phases/snapshot/docs.md#-advice-variant)
+ */
+type Advice = u8 & { _not_real: "advice" };
+
+/** The application has no advice to give on its behavior with respect to the specified data. */
+const ADVICE_NORMAL     = <Advice>0;
+
+/** The application expects to access the specified data sequentially from lower offsets to higher offsets. */
+const ADVICE_SEQUENTIAL = <Advice>1;
+
+/** The application expects to access the specified data in a random order. */
+const ADVICE_RANDOM     = <Advice>2;
+
+/** The application expects to access the specified data in the near future. */
+const ADVICE_WILLNEED   = <Advice>3;
+
+/** The application expects that it will not access the specified data in the near future. */
+const ADVICE_DONTNEED   = <Advice>4;
+
+/** The application expects to access the specified data once and then not reuse it thereafter. */
+const ADVICE_NOREUSE    = <Advice>5;
