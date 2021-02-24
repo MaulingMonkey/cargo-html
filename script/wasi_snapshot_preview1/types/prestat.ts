f02a6f1a26c2d25503ac858e95235d90eaa2fc4c
MaulingMonkey <git@maulingmonkey.com>
2021-02-24 02:09:57 -0800

    PreStat: document

--- script/wasi_snapshot_preview1/types/prestat.ts
+++ script/wasi_snapshot_preview1/types/prestat.ts
@@ -1,7 +1,15 @@
+/** \[[WASI](https://github.com/WebAssembly/WASI/blob/main/phases/snapshot/docs.md#-preopentype-variant)\] Identifiers for preopened capabilities. */
 type PreOpenType = u8 & { _not_real: "preopentype"; }
+
+/** A pre-opened directory. */
 const PREOPENTYPE_DIR = <PreOpenType>0;
 
+
+
+/** \[[WASI](https://github.com/WebAssembly/WASI/blob/main/phases/snapshot/docs.md#-prestat-variant)\] Information about a pre-opened capability. */
 interface PreStat {
     tag:                PreOpenType;
+
+    /** \[[WASI](https://github.com/WebAssembly/WASI/blob/main/phases/snapshot/docs.md#prestat_dir)\] The length of the directory name for use with fd_prestat_dir_name. */
     u_dir_pr_name_len?: usize; // PREOPENTYPE_DIR
 }
