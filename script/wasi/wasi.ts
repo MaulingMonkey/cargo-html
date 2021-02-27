91f2469635e68ac1c60fab25f87819a7ed49a5d8
MaulingMonkey <git@maulingmonkey.com>
2021-02-26 20:31:36 -0800

    document wasi namespace

---
+++ script/wasi/wasi.ts
@@ -0,0 +1,6 @@
+/**
+ * Types, functions, and utilities to build browser-friendly [WASI](https://wasi.dev/) implementations.
+ *
+ * This currently focuses on [`wasi_snapshot_preview1`](https://github.com/WebAssembly/WASI/blob/main/phases/snapshot/docs.md).
+ */
+namespace wasi {}
