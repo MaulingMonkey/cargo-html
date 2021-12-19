1fc63a7e9e083a76c2e861f1bca9d6e9d88a4fb6
MaulingMonkey <git@maulingmonkey.com>
2021-12-18 18:07:38 -0800

    Misc. fixes continued: generate HTML for unk2 targets (forgot to commit due to unrelated changes...)

--- src/build/html.rs
+++ src/build/html.rs
@@ -17,59 +17,66 @@ pub(crate) fn pages(args: &Arguments, metadata: &Metadata) {
         let target_html_dir = metadata.target_directory().join("cargo-html").join(config.as_str());
         std::fs::create_dir_all(&target_html_dir).unwrap_or_else(|err| fatal!("unable to create `{}`: {}", target_html_dir.display(), err));
 
-        let target_arch_config_dir = metadata.target_directory().join("wasm32-wasi").join(config.as_str());
-        for (ty, target, pkg) in metadata.selected_targets_wasi() {
-            let target_arch_config_dir = match ty {
-                TargetType::Bin     => target_arch_config_dir.clone(),
-                TargetType::Example => target_arch_config_dir.join("examples"),
-                TargetType::Cdylib  => continue, // XXX?
-            };
+        for (arch, filter) in [
+            ("wasm32-wasi",             (|pkg: &Package| pkg.is_wasi) as fn(&Package) -> bool),
+            ("wasm32-unknown-unknown",  |pkg: &Package| pkg.is_wasm_unk2),
+        ].iter().copied() {
+            let target_arch_config_dir = metadata.target_directory().join(arch).join(config.as_str());
+            for (ty, target, pkg) in metadata.selected_targets() {
+                if !filter(pkg) { continue }
 
-            let mut template_js = String::new();
-            if pkg.wasm_bindgen.is_some() {
-                let js_dir = target_arch_config_dir.join("js");
-                js::inline_wasm_bindgen_bundler_importer(&mut template_js, "__cargo_html_wasmbindgen_bundler_js", &js_dir, target).unwrap_or_else(|err| fatal!("unable to reprocess wasm-bindgen javascript: {}", err));
-                template_js.push_str("\r\n");
+                let target_arch_config_dir = match ty {
+                    TargetType::Bin     => target_arch_config_dir.clone(),
+                    TargetType::Example => target_arch_config_dir.join("examples"),
+                    TargetType::Cdylib  => continue, // XXX?
+                };
+
+                let mut template_js = String::new();
+                if pkg.wasm_bindgen.is_some() {
+                    let js_dir = target_arch_config_dir.join("js");
+                    js::inline_wasm_bindgen_bundler_importer(&mut template_js, "__cargo_html_wasmbindgen_bundler_js", &js_dir, target).unwrap_or_else(|err| fatal!("unable to reprocess wasm-bindgen javascript: {}", err));
+                    template_js.push_str("\r\n");
+                }
+                template_js.push_str(include_str!("../../template/script.js"));
+
+                let wasm = if pkg.asyncify {
+                    target_arch_config_dir.join(format!("{}.async.wasm", target))
+                } else if pkg.wasm_bindgen.is_some() {
+                    target_arch_config_dir.join("js").join(format!("{}_bg.wasm", target))
+                } else {
+                    target_arch_config_dir.join(format!("{}.wasm", target))
+                };
+                generate(pkg, &target_html_dir, target, config, "console", &template_js, &wasm);
             }
-            template_js.push_str(include_str!("../../template/script.js"));
 
-            let wasm = if pkg.asyncify {
-                target_arch_config_dir.join(format!("{}.async.wasm", target))
-            } else if pkg.wasm_bindgen.is_some() {
-                target_arch_config_dir.join("js").join(format!("{}_bg.wasm", target))
-            } else {
-                target_arch_config_dir.join(format!("{}.wasm", target))
-            };
-            generate(pkg, &target_html_dir, target, config, "console", &template_js, &wasm);
-        }
-
-        let target_arch_config_dir  = metadata.target_directory().join("wasm32-unknown-unknown").join(config.as_str());
-        for (ty, target, pkg) in metadata.selected_targets_cargo_web() {
-            let target_arch_config_dir = match ty {
-                TargetType::Bin     => target_arch_config_dir.clone(),
-                TargetType::Example => target_arch_config_dir.join("examples"),
-                TargetType::Cdylib  => continue,
-            };
-            let package_js = target_arch_config_dir.join(format!("{}.js", target));
-            let package_js = std::fs::read_to_string(&package_js).unwrap_or_else(|err| fatal!("unable to read `{}`: {}", package_js.display(), err));
-            let package_js = include_str!("../../template/js/cargo-web.js").replace("{PACKAGE_JS}", &package_js);
-            let wasm = target_arch_config_dir.join(format!("{}.wasm", target));
-            generate(pkg, &target_html_dir, target, config, "basic", &package_js, &wasm);
-        }
-
-        let pkg_dir = metadata.target_directory().join("wasm32-unknown-unknown").join(config.as_str()).join("pkg");
-        for (ty, target, pkg) in metadata.selected_targets_wasm_pack() {
-            match ty {
-                TargetType::Bin     => continue,
-                TargetType::Example => continue,
-                TargetType::Cdylib  => {},
+            let target_arch_config_dir  = metadata.target_directory().join("wasm32-unknown-unknown").join(config.as_str());
+            for (ty, target, pkg) in metadata.selected_targets_cargo_web() {
+                let target_arch_config_dir = match ty {
+                    TargetType::Bin     => target_arch_config_dir.clone(),
+                    TargetType::Example => target_arch_config_dir.join("examples"),
+                    TargetType::Cdylib  => continue,
+                };
+                let package_js = target_arch_config_dir.join(format!("{}.js", target));
+                let package_js = std::fs::read_to_string(&package_js).unwrap_or_else(|err| fatal!("unable to read `{}`: {}", package_js.display(), err));
+                let package_js = include_str!("../../template/js/cargo-web.js").replace("{PACKAGE_JS}", &package_js);
+                let wasm = target_arch_config_dir.join(format!("{}.wasm", target));
+                generate(pkg, &target_html_dir, target, config, "basic", &package_js, &wasm);
             }
-            let lib_name = target.replace("-", "_");
-            let package_js = pkg_dir.join(format!("{}.js", lib_name));
-            let package_js = std::fs::read_to_string(&package_js).unwrap_or_else(|err| fatal!("unable to read `{}`: {}", package_js.display(), err));
-            let package_js = include_str!("../../template/js/wasm-pack.js").replace("{PACKAGE_JS}", &package_js);
-            let wasm = pkg_dir.join(format!("{}_bg.wasm", lib_name));
-            generate(pkg, &target_html_dir, target, config, "basic", &package_js, &wasm);
+
+            let pkg_dir = metadata.target_directory().join("wasm32-unknown-unknown").join(config.as_str()).join("pkg");
+            for (ty, target, pkg) in metadata.selected_targets_wasm_pack() {
+                match ty {
+                    TargetType::Bin     => continue,
+                    TargetType::Example => continue,
+                    TargetType::Cdylib  => {},
+                }
+                let lib_name = target.replace("-", "_");
+                let package_js = pkg_dir.join(format!("{}.js", lib_name));
+                let package_js = std::fs::read_to_string(&package_js).unwrap_or_else(|err| fatal!("unable to read `{}`: {}", package_js.display(), err));
+                let package_js = include_str!("../../template/js/wasm-pack.js").replace("{PACKAGE_JS}", &package_js);
+                let wasm = pkg_dir.join(format!("{}_bg.wasm", lib_name));
+                generate(pkg, &target_html_dir, target, config, "basic", &package_js, &wasm);
+            }
         }
     }
 }
